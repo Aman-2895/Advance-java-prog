6718657d3e2352d6d21c227dfdfa6e667802d889
Aman Goyal <163448015+Aman-2895@users.noreply.github.com>
2026-03-24 20:18:21 +0530

    Add MainTextBlock class with main method

---
+++ MainTextBlock.java
@@ -0,0 +1,29 @@
+package abc;
+
+public class MainTextBlock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String data = """ 
+	package abc;
+public class MainVar {
+   public static void main(String[] args) {
+	   int a=10;
+	   // initialisation is must
+	   // not initialised by null data type is must
+	   var b =20;
+	   var name = "Aman";                   // local variable type infrance
+	   String name1 = new String("this is string");
+	   var name2 = new String("this is string with var");
+	   
+	   int []arr = {1,2,3,4,5};
+	   for( var nums: arr) {
+		   System.out.println(nums);
+	   }
+   }
+}
+				""";	
+		System.out.println(data);
+
+	}
+}
